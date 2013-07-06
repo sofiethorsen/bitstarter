b1eee3edbddb46fff1fa3ea29cd16a3e34854ee6
Sofie Thorsen <thorsen@spotify.com>
2013-07-06 20:09:50 +0200

    Add index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var message = fs.readFileSync('index.html');
+var buffer = new Buffer(message);
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
